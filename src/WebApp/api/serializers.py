fcb1eee7e08b064bd7af3ec4eb13ca175e3ae931
lukehollinda <luke.hollinda@gmail.com>
2021-04-16 01:57:51 -0600

    Updated front end

--- src/WebApp/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Land
-
-
-class LandSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Land
-        fields = ('id', 'address', 'postalCode', 'sqrAcres')
