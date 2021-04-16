1ad0c8e13eac119898e8981f73e864fd0ef02a7f
lukehollinda <luke.hollinda@gmail.com>
2021-04-15 19:04:19 -0600

    Created database and API

--- src/WebApp/WebApp/urls.py
+++ src/WebApp/WebApp/urls.py
@@ -17,16 +17,29 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from api import views
+from api import views #REMOVE WEBAPP
 #from WebApp.pages.views import home_view
 
 router = routers.DefaultRouter()
-router.register(r'api', views.LandViewSet, 'land')
+#router.register(r'api', views.LandViewSet, 'land')
+router.register(r'city', views.CityViewSet, 'City')
+router.register(r'neighborhood', views.NeighborhoodViewSet, "Neighborhood")
+router.register(r'land', views.LandViewSet, 'Land')
+router.register(r'developer', views.DeveloperViewSet, 'Developer')
+router.register(r'owner', views.OwnerViewSet, 'Owner')
+router.register(r'personOwner', views.PersonOwnerViewSet, 'PersonOwner')
+router.register(r'companyOwner', views.CompanyOwnerViewSet, 'CompanyOwner')
+router.register(r'bankOwner', views.BankOwnerViewSet, 'BankOwner')
+router.register(r'ownsRelation', views.OwnsRelationViewSet, 'OwnsRelation')
+router.register(r'building', views.BuildingViewSet, 'Building')
+router.register(r'commercialBuilding', views.CommecialBuildingViewSet, 'CommercialBuilding')
+router.register(r'residentialBuilding', views.ResidentialBuildingViewSet, 'ResidentialBuilding')
+router.register(r'renter', views.RenterViewSet, 'Renter')
 
 urlpatterns = [
    # path('', home_view, name = 'home'),
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('api/', include(router.urls))
+    #path('', include('api.urls')),
+    path('', include(router.urls))
 
 ]
--- src/WebApp/api/serializers.py
+++ src/WebApp/api/serializers.py
@@ -1,8 +1,69 @@
 from rest_framework import serializers
-from .models import Land
+from .models import *
 
 
-class LandSerializer(serializers.HyperlinkedModelSerializer):
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ('name', 'latitude', 'longitude')
+
+class NeighborhoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Neighborhood
+        fields = ('name', 'cityName')
+
+class LandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Land
-        fields = ('id', 'address', 'postalCode', 'sqrAcres')
+        fields = ('address', 'postalCode', 'sqrAcres', "neighborhoodName")
+
+class DeveloperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Developer
+        fields = ('companyName', 'headQuarters')
+    
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Owner
+        fields = ('ownerId', 'email', 'phoneNumber')
+
+class PersonOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonOwner
+        fields = ('ownerId', 'firstName', 'middleName', 'lastName')
+
+class CompanyOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyOwner
+        fields = ('ownerId', 'companyName', 'companyType')
+    
+class BankOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankOwner
+        fields = ('ownerId', 'bankName', 'headQuarters')
+
+class OwnsRelationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OwnsRelation
+        fields = ('ownerId', 'land')
+
+class BuildingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Building
+        fields = ('land', 'developer', 'squareFootage', 'numberOfStories')
+
+class CommercialBuildingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommercialBuilding
+        fields = ('building', 'industryType', 'numberOfDesks')
+
+class ResidentialBuildingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResidentialBuilding
+        fields = ('building', 'numberOfBathrooms', 'numberOfBedrooms')
+
+class RenterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Renter
+        fields = ('renterId', 'building', 'firstName', 'middleName', \
+                    'lastName', 'phoneNumber', 'email', 'costPerMonth', 'leaseDuration' )    
